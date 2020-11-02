b4f8153b505a0634200cca596d4b004c3ee03084
jhern311 <jhern311@yahoo.com>
2020-11-01 22:42:33 -0800

    updating API homework

--- python-api-challenge/api_keys.py
+++ python-api-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "007fe7bad8efec01e08f6134c526f0b3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD_TBkhsarXgqBQycqCBchlIg4Ahs4vTBY"
